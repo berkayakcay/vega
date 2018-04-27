b3f561a641d3e9bd4734d4ea9a329f1c449d070d
Berkay AKÇAY <berkayakcay@outlook.com>
2018-04-27 08:28:13 +0300

    in

--- Controllers/MakeController.cs
+++ Controllers/MakeController.cs
@@ -7,6 +7,7 @@ namespace vega.Controllers
     public class MakeController
     {
         
+        
         public MakeController()
         {
             
--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace vega
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
 
+
         }
 
         public IConfiguration Configuration { get; }
