599bbaed6ced4f34e542d92cce1661c90f55676f
Berkay AKÇAY <berkayakcay@outlook.com>
2018-04-27 08:23:45 +0300

    in

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,9 +1,14 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace vega.Models
 {
     public class Make
     {
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
         public  int Id { get; set; }
         public string Name { get; set; }
 
